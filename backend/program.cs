cb8d91bccbd82a7e1bd77e8ade35bdab1db35ae1
Nadont02 <dunyushkin2007@mail.ru>
2026-01-13 13:42:15 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Дунюшкин Н.С");
+Console.WriteLine("А меня зовут Салалыкина О.М");
+DateTime now = DateTime.Now;
+Console.WriteLine(now.ToString("MM-dd-yyyy HH:mm:ss"));
